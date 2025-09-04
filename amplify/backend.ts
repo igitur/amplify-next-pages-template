b0b25561334563e8da46fe1785e6db0859a02c9e
Francois Botha <igitur@gmail.com>
2025-09-04 18:02:10 +0200

    Add permissions boundary for iam roles

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,25 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { Aspects, CfnResource, Stack } from 'aws-cdk-lib';
+import { IConstruct } from 'constructs';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const rootScope = Stack.of(backend.auth.resources.userPool).node.scope
+
+if (rootScope) {
+  Aspects.of(rootScope).add({
+    visit(node: IConstruct) {
+      if (
+          CfnResource.isCfnResource(node) &&
+          (node.cfnResourceType == 'AWS::IAM::Role' || node.cfnResourceType == 'AWS::IAM::User')
+      ) {
+        node.addPropertyOverride('PermissionsBoundary', 'arn:aws:iam::140023385708:policy/permission-boundary');
+      }
+    },
+  });
+}
